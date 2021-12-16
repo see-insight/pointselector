e31d99b193dee91b85c918bae9ef5cbaece96f85
Dirk Colbry <colbrydi@msu.edu>
2021-12-16 15:59:06 -0500

    Making command line point selection program

--- PointSelection.py
+++ PointSelection.py
@@ -8,6 +8,33 @@ import matplotlib.image as mpimg
 import ipywidgets as widgets
 import argparse
 
+
+def detectPoints(event,x,y,flags,params):
+    global circles
+    global stop
+    if event == cv2.EVENT_LBUTTONDOWN:
+        circles.append([x,y])
+    if event == cv2.EVENT_RBUTTONDOWN:
+        stop = True
+
+def pickpoints(image, n=9999):
+    global circles
+    global stop
+    circles = []
+    stop = False
+    half_window = 100
+    while stop == False:
+        for point in circles:
+            cv2.circle(image,(point[0], point[1]),3,(0,255,0),cv2.FILLED)    
+        cv2.imshow("Original", image)
+        cv2.setMouseCallback("Original", detectPoints)
+        cv2.waitKey(1)
+        if len(circles) >= n:
+            stop = True
+    
+    for point in circles:
+        print(f"{point[0]}, {point[1]}")
+        
 #TODO: Add a "done" button. (or something)
 
 #TODO: Remove -f option and require a string as input. 
@@ -18,56 +45,27 @@ import argparse
 
 #TODO: Allow for a folder in addition to a name then loop over images in folder.
 
-parser = argparse.ArgumentParser(description='Annotate Point Data')
-parser.add_argument('-f', '--file', default='balloonfiesta.jpeg',
-                    help="input filename")
-# Parsing Inputs
-args = parser.parse_args()
-filename = args.file
+#TODO: Move argparse stuff into a __name__=="__main__" section
 
-#TODO Fix hard coding of image size
-image = cv2.imread(filename)
-resized_image = cv2.resize(image, (800,500))
-circles = np.zeros((1,2), int)
-counter = 0
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Annotate Point Data')
+    parser.add_argument('-f', '--file', default='balloonfiesta.jpeg',
+                        help="input filename")
+    parser.add_argument('-n', '--number', default=9999,
+                        help="number of points to select")
+    # Parsing Inputs
+    args = parser.parse_args()
+    filename = args.file
 
-
-def detectPoints(event,x,y,flags,params):
-    global counter
-    if event == cv2.EVENT_LBUTTONDOWN:
-        circles[counter] = x,y
-        counter = counter + 1
-        print(circles)
-        
-# cid = fig.canvas.mpl_connect('button_press_event', detectPoints)
-
-# imgplot = plt.imshow(image)
-# plt.show()
-
-
-
-
-half_window = 100
-counter = 0
-while True:
-    if counter == 1:
-        #TODO: add if statemetns to make sure index is not less than zero
-        template = resized_image[circles[0,1]-half_window:circles[0,1]+half_window, circles[0,0]-half_window:circles[0,0]+half_window, :]
-        cv2.imshow("Output Image", template)
-        counter = 0
-        
-    cv2.imshow("Original", resized_image)
-    cv2.setMouseCallback("Original", detectPoints)
-    cv2.waitKey(1)
-
+    #TODO Fix hard coding of image size
+    image = cv2.imread(filename)
+    resized_image = cv2.resize(image, (800,500))
+    pickpoints(resized_image, int(args.number))
     
-#TODO: Export point locations as a file
-#TODO:  ex:  Street.png  10 20 Corner_of_Eye
-#            Street.png  399 19 Nose_tip
-
-# Simple example:
-#    10 20 point1
-#    399 19 point2
-
-# if __name__ == "__main__":
-
+        
+        
+        
+        
+        
+        
+        
